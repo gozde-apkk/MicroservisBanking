65bcd047ecd1adb5e535c3cc70087ce5b6f88cb7
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-13 16:42:28 +0300

    created customer service

---
+++ src/API/MsBanking.Core/Services/CustomerService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MsBanking.Common.Entity;
+using MsBanking.Core.Domain;
+
+namespace MsBanking.Core.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly IMongoCollection<Customer> customerCollection;
+
+        public CustomerService(IOptions<DatabaseOption> options)
+        {
+            var dbOptions = options.Value;
+            var client = new MongoClient(dbOptions.ConnectionString);
+            var database = client.GetDatabase(dbOptions.DatabaseName);
+            customerCollection = database.GetCollection<Customer>(dbOptions.CustomerCollectionName);
+        }
+
+
+        public async Task<Customer> GetCustomer(int id)
+        {
+            var customerEntity = await customerCollection.FindAsync(c => c.Id == id);
+            return customerEntity.FirstOrDefault();
+        }
+
+        public async Task<List<Customer>> GetCustomers()
+        {
+            var customerEntities = await customerCollection.FindAsync(c => true);
+            return customerEntities.ToList();
+        }
+
+        public async Task<Customer> CreateCustomer(Customer customer)
+        {
+            await customerCollection.InsertOneAsync(customer);
+            return customer;
+        }
+    }
+}
