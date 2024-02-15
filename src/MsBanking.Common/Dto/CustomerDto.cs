03afd25ea316b1849bf4930092021d00308c6e87
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-15 14:37:22 +0300

    customerdto created

---
+++ src/MsBanking.Common/Dto/CustomerDto.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MsBanking.Common.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsBanking.Common.Dto
+{
+    public class CustomerDto
+    {
+        public string? FullName { get; set; }
+
+        public long? CitizenNumber { get; set; }
+        public string? Email { get; set; }
+
+        public DateTime BirthDate { get; set; } = DateTime.MinValue;
+    }
+
+    public class CustomerDtoProfile : Profile
+    {
+
+        public CustomerDtoProfile ()
+        {
+            CreateMap<CustomerDto, Customer>().ReverseMap();
+            CreateMap<CustomerResponseDto, Customer>().ReverseMap();
+        }
+    }
+
+    public class CustomerResponseDto : CustomerDto
+    {
+        public int Id { get; set; } 
+    }
+}
