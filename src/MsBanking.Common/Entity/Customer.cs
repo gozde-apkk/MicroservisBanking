ae4a8a7942d77815b4157a55dd382639255ece85
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-15 14:38:05 +0300

    created entities

--- src/MsBanking.Common/Entity/Customer.cs
+++ src/MsBanking.Common/Entity/Customer.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace MsBanking.Common.Entity
 {
-    public class Customer
+    public class Customer : BaseEntity  
     {
         [BsonId]
         [BsonRepresentation(MongoDB.Bson.BsonType.Int32)]
