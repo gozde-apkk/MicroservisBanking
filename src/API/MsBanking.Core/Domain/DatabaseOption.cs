f27cce12f642c22fdafdc8c8f3c8d89a1aaf7f59
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-19 13:17:20 +0300

    files deleted

--- src/API/MsBanking.Core/Domain/DatabaseOption.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MsBanking.Core.Domain
-{
-    public class DatabaseOption
-    {
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-        public string CustomerCollectionName { get; set; }
-    }
-}
