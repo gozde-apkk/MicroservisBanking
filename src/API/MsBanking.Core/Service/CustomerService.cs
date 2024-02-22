566b61b4b1f5832ac5beb9121f3d3ec49535978c
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-22 13:58:21 +0300

    created  customer service

---
+++ src/API/MsBanking.Core/Service/CustomerService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MsBanking.Common.Entity;
+using MsBanking.Core.Domain;
+
+namespace MsBanking.Core.Service
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly IMongoCollection<Customer> customerCollection;
+
+        public CustomerService(IOptions<DatabaseOptions> options)
+        {
+            var dbOptions = options.Value;
+            var client = new MongoClient(dbOptions.ConnectionString);//bağlantı
+            var database = client.GetDatabase(dbOptions.DatabaseName);//veritabanı
+            customerCollection = database.GetCollection<Customer>(dbOptions.CustomerCollectionName);//tablo
+        }
+
+        public async Task<Customer> GetCustomer(int id)
+        {
+            var customer = await customerCollection.FindAsync(c => c.Id == id);
+            return customer.FirstOrDefault();
+        }
+
+        public async Task<List<Customer>> GetCustomers()
+        {
+            var customers = await customerCollection.FindAsync(c => true);
+            return customers.ToList();
+        }
+
+        public async Task<Customer> CreateCustomer(Customer customer)
+        {
+            await customerCollection.InsertOneAsync(customer);
+            return customer;
+        }
+    }
+}
---
+++ src/API/MsBanking.Core/Service/ICustomerService.cs
@@ -0,0 +1,11 @@
+﻿using MsBanking.Common.Entity;
+
+namespace MsBanking.Core.Service
+{
+    public interface ICustomerService
+    {
+        Task<Customer> CreateCustomer(Customer customer);
+        Task<Customer> GetCustomer(int id);
+        Task<List<Customer>> GetCustomers();
+    }
+}
