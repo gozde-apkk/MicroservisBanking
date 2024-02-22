159801a1a66f19ab7cfe00402e5005959549e829
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-22 14:00:23 +0300

    program.cs resolve service

--- src/API/MsBanking.Core/Program.cs
+++ src/API/MsBanking.Core/Program.cs
@@ -1,50 +1,33 @@
-
-using MsBanking.Common.Dto;
 using MsBanking.Core.Apis;
 using MsBanking.Core.Domain;
-using MsBanking.Core.Services;
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
+using MsBanking.Core.Service;
 
-namespace MsBanking.Core
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.Configure<DatabaseOptions>(builder.Configuration.GetSection("DatabaseOptions"));
+
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddAuthorization();
-
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-
-            builder.Services.Configure<DatabaseOption>(builder.Configuration.GetSection("DatabaseOptions"));
-            builder.Services.AddScoped<ICustomerService, CustomerService>();
-
-
-            builder.Services.AddAutoMapper(typeof(CustomerDtoProfile));  
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-            app.UseOcelot();    
-            app.MapGroup("api/v1/")
-                .WithTags("Core Banking Api v1")
-                .MapCustomerApi();
-
-            app.Run();
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.MapGroup("/api/v1")
+    .WithTags("Banking api v1")
+    .MapCustomerApi();
+app.Run();
+
+
