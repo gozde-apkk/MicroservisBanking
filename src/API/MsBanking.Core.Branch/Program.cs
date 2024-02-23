c0c197c61834365c638829167d07e6ff8bb614f0
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-23 12:07:13 +0300

    Microsoft.Extensions.Caching.StackExchangeRedis package added

--- src/API/MsBanking.Core.Branch/Program.cs
+++ src/API/MsBanking.Core.Branch/Program.cs
@@ -1,6 +1,9 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using MsBanking.Common.Dto;
 using MsBanking.Core.Branch;
+using MsBanking.Core.Branch.Api;
 using MsBanking.Core.Branch.Domain;
+using MsBanking.Core.Branch.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,9 +15,28 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
-
+//sql server
 builder.Services.AddDbContext<BranchDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+//resolve service
+builder.Services.AddScoped<IBranchService, BranchService>();
+
+
+//automapper
+builder.Services.AddAutoMapper(typeof(BranchProfile));
+
+
+//redis
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+    options.InstanceName = "MsBanking.Core.Branch";
+});
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,12 +45,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 
-app.MapControllers();
-
+app.MapGroup("api/vı/")
+    .WithTags("Branch")
+    .MapBranchApi();
 
 DataSeeder.Seed(app);
+
 app.Run();
