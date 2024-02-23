a34dd894277b1502219a3048344fdda8f99eb916
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-23 17:19:31 +0300

    accountdto updated

--- src/MsBanking.Common/Dto/AccountDto.cs
+++ src/MsBanking.Common/Dto/AccountDto.cs
@@ -10,7 +10,7 @@ namespace MsBanking.Common.Dto
     {
         public string AccountNumber { get; set; }
         public string IbanNumber { get; set; }
-        public int AccountType { get; set; } //Bireysel & Tüzel
+        public int AccountType { get; set; }
         public decimal Balance { get; set; }
         public string Currency { get; set; }
         public string UserId { get; set; }
