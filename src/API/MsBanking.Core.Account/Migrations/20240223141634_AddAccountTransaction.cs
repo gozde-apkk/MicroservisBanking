2bc147d88a98e7a5875906f54afba857b847dc9c
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-24 11:35:36 +0300

    account migrations file

---
+++ src/API/MsBanking.Core.Account/Migrations/20240223141634_AddAccountTransaction.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MsBanking.Core.Account.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccountTransaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Accounts",
+                table: "Accounts");
+
+            migrationBuilder.RenameTable(
+                name: "Accounts",
+                newName: "Account");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Account",
+                table: "Account",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "AccountTransaction",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountId = table.Column<int>(type: "int", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TransactionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TransactionType = table.Column<int>(type: "int", nullable: false),
+                    ToAccountId = table.Column<int>(type: "int", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    isActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountTransaction", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AccountTransaction_Account_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Account",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountTransaction_AccountId",
+                table: "AccountTransaction",
+                column: "AccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccountTransaction");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Account",
+                table: "Account");
+
+            migrationBuilder.RenameTable(
+                name: "Account",
+                newName: "Accounts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Accounts",
+                table: "Accounts",
+                column: "Id");
+        }
+    }
+}
