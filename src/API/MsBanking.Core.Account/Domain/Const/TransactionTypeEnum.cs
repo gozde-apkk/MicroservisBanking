b3e5f9cb4f25fd05a9543e2e9f0e6a5a9ea2b12c
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-24 13:38:30 +0300

    created transaction enum type

--- src/API/MsBanking.Core.Account/Domain/Const/TransactionTypeEnum.cs
+++ src/API/MsBanking.Core.Account/Domain/Const/TransactionTypeEnum.cs
@@ -1,15 +1,17 @@
 ﻿namespace MsBanking.Core.Account.Domain.Const
 {
-    public enum TransactionTypeEnum
-    {
-        Deposit = 1,
-        Withdraw = 2,
-        Transfer = 3
-    }
+    
+        public enum TransactionTypeEnum
+        {
+            Deposit = 1,
+            Withdraw = 2,
+            Transfer = 3
+        }
 
-    public enum AccountTypeEnum
-    {
-        Retail = 1,
-        Corporate = 2
-    }
+        public enum AccountTypeEnum
+        {
+            Retail = 1,
+            Corporate = 2
+        }
+    
 }
